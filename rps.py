b952037d311d9b9b30515159006e21b98cb19a0f
eshiiiac <134635429+eshiiiac@users.noreply.github.com>
2024-03-30 21:09:53 +0545

    Update rps.py

--- rps.py
+++ rps.py
@@ -1,35 +1,54 @@
 import random
 
-cChoice =random.choice(['rock', 'paper', 'scissor'])
 
-def compChoice():
+print(
+    '''
+    -----------------------------------
+    | WELCOME TO ROCK PAPER SCISSOR!! |
+    -----------------------------------
+    '''
+)
+while True:
     cChoice =random.choice(['rock', 'paper', 'scissor'])
-
-userChoice= input("Enter Rock(r/R)  Paper(p/P) or Scissor(s/S): ")
-print(userChoice)
-print(cChoice)
-
-
-if(userChoice == cChoice):
-    print("Its a tie")
-
-elif(userChoice=="r" or userChoice =="R " and compChoice=="scissor" ):
-    print("user wins")
-
-elif(userChoice=="r" or userChoice =="R" and compChoice=="paper"):
-    print("computer wins")
-
-elif(userChoice=="p" or userChoice =="P" and compChoice=="scissor"):
-    print("computer wins")
-
-elif(userChoice=="p" or userChoice =="P" and compChoice=="rock"):
-    print("user wins")
-
-elif(userChoice=="s" or userChoice =="S" and compChoice=="rock"):
-    print("computer wins")
-
-elif(userChoice=="s" or userChoice =="S" and compChoice=="paper"):
-    print("user wins")
+    userChoice= input("Enter your choice: ").lower()
+    print(" ----------------------------------- ")
+    print("Computer's Choice:--",cChoice)
+    print(" ----------------------------------- ")
+    print( "Your Choice:--------",userChoice)
+    print(" ----------------------------------- ")
     
+    if(userChoice == cChoice):
+        print(" | Its a tie |")
+        print(" ----------------------------------- ")
+        continue
 
+    elif(userChoice=="rock" and cChoice=="scissor" ):
+        print("| You win |")
+        print(" ----------------------------------- ")
+        continue
 
+    elif(userChoice=="rock" and cChoice=="paper"):
+        print("| computer wins |")
+        print(" ----------------------------------- ")
+        continue
+
+    elif(userChoice=="paper" and cChoice=="scissor"):
+        print("| computer wins |")
+        print(" ----------------------------------- ")
+        continue
+
+    elif(userChoice=="paper" and cChoice=="rock"):
+        print("| You win |")
+        print(" ----------------------------------- ")
+        continue
+
+    elif(userChoice=="scissor" and cChoice=="rock"):
+        print("| computer wins |")
+        print(" ----------------------------------- ")
+        continue
+
+    elif(userChoice=="scissor" and cChoice=="paper"):
+        print("| You win |")
+        print(" ----------------------------------- ")
+        continue
+    
